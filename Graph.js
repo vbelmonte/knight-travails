1e47175d7b141a8c6d728d9f2a2a7270b1dbf2e8
Villy Belmonte <vjbbelmont@gmail.com>
2024-02-09 14:49:44 -0800

    Modify Graph class

--- Graph.js
+++ Graph.js
@@ -1,65 +1,33 @@
-import Node from "./Node.js";
-
 export default class Graph {
-  matrix = [];
+  adjList = null;
 
-  vertices = [];
+  numOfVertices = null;
 
-  constructor(numberOfNodes) {
-    for (let i = 0; i < numberOfNodes; i += 1) {
-      this.matrix.push(new Array(numberOfNodes).fill(0));
-    }
+  constructor(numberOfVertices) {
+    this.adjList = new Array(numberOfVertices).fill([]);
+    this.numOfVertices = numberOfVertices;
 
-    for (let i = 0; i < numberOfNodes; i += 1) {
-      const node = new Node(i);
-      this.vertices.push(node);
+    for (let i = 0; i < numberOfVertices; i += 1) {
+      this.adjList[i] = [];
     }
   }
 
-  get matrix() {
-    return this._matrix;
+  get adjList() {
+    return this._adjList;
+  }
+
+  get numOfVertices() {
+    return this._numOfVertices;
   }
 
   addEdge(source, destination) {
-    this.matrix[source][destination] = 1;
-    this.matrix[destination][source] = 1;
-  }
-
-  removeEdge(source, destination) {
-    this.matrix[source][destination] = 0;
-    this.matrix[destination][source] = 0;
-  }
-
-  isEdge(source, destination) {
-    if (this.matrix[source][destination] === 1) {
-      return true;
-    }
-    return false;
+    this.adjList[source].push(destination);
+    /* this.adjList[destination].push(source); */
   }
 
   printGraph() {
-    for (let i = 0; i < this.matrix.length; i += 1) {
-      console.log(this.matrix[i]);
+    for (let i = 0; i < this.adjList.length; i += 1) {
+      console.log(this.adjList[i]);
     }
-  }
-
-  updateVertices(source, destination) {
-    if ((destination - source) === 1) {
-      this.vertices[source].rightNode = this.vertices[destination];
-      this.vertices[destination].leftNode = this.vertices[source];
-    } else if (Math.abs(destination - source) === 1) {
-      this.vertices[source].leftNode = this.vertices[destination];
-      this.vertices[destination].rightNode = this.vertices[source];
-    } else if ((destination - source) === 8) {
-      this.vertices[source].bottomNode = this.vertices[destination];
-      this.vertices[destination].topNode = this.vertices[source];
-    } else if (Math.abs(destination - source) === 8) {
-      this.vertices[source].topNode = this.vertices[destination];
-      this.vertices[destination].bottomNode = this.vertices[source];
-    }
-  }
-
-  getVertex(vertex) {
-    return this.vertices[vertex];
   }
 }
